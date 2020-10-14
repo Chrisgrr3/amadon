63555decdad0de310b52bebbc23949ccd8839bdb
Christopher Guerrero <71108741+Chrisgrr3@users.noreply.github.com>
2020-10-14 01:20:29 -0700

    Fixed issues described in README

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Order, Product
 
 def index(request):
@@ -9,8 +9,23 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    purchased_item_id = request.POST['product_id']
+    price_of_item = float(Product.objects.get(id=purchased_item_id).price)
+    total_charge = quantity_from_form * price_of_item
+    request.session['total_charge'] = total_charge
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    if 'total_spent' not in request.session:
+        request.session['counter'] = 0
+        request.session['total_spent'] = 0
+    request.session['counter'] += quantity_from_form
+    request.session['total_spent'] += total_charge
+    return redirect("/checkout_page")
+
+def only_summary(request):
+    context = {
+        'total_charge': request.session['total_charge'],
+        'current_count': request.session['counter'],
+        'total_spent': request.session['total_spent']
+    }
+    return render(request,'store/checkout.html', context)
